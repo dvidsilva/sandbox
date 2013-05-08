31da4289965d03264e0188c9207b2c4132a2f0b4
Dvid Silva <contacto@dvidsilva.com>
2013-05-08 16:10:05 -0500

    Initial Commit

---
+++ sandbox/DVDAppDelegate.h
@@ -0,0 +1,19 @@
+//
+//  DVDAppDelegate.h
+//  sandbox
+//
+//  Created by Dvid Silva on 5/8/13.
+//  Copyright (c) 2013 Dvid Silva. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@class DVDViewController;
+
+@interface DVDAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@property (strong, nonatomic) DVDViewController *viewController;
+
+@end
